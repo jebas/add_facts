47d738bd004da4eff68918e2657c9b0dcd7eb3a5
Jeff Baskin <jeff.l.baskin@gmail.com>
2016-05-23 21:20:03 -0400

    Made the test work for multiple entries.

--- spec/acceptance/entry_spec.rb
+++ spec/acceptance/entry_spec.rb
@@ -5,14 +5,18 @@ require 'spec_helper_acceptance'
 describe 'add_facts entry' do
   it 'should run with no errors' do
     pp = <<-EOS
-      add_facts::entry { 'foo':
-        value => 'bar',
+      add_facts::entry { 'fred':
+        value => 'wilma',
+      }
+      add_facts::entry { 'barney':
+        value => 'betty',
       }
     EOS
     expect(apply_manifest(pp).exit_code).to_not eq(1)
     expect(apply_manifest(pp).exit_code).to eq(0)
   end
   describe file('/opt/puppetlabs/facter/facts.d/add_facts.txt') do
-    its(:content) {should match /foo = bar/}
+    its(:content) {should match /fred = wilma/}
+    its(:content) {should match /barney = betty/}
   end
 end
